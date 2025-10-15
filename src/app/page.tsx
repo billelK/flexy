c0c188269f609c194860b2aeaa6da157be633096
billelK <bilalkhansal@gmail.com>
2025-10-15 09:28:49 +0100

    header wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import RechargeForm from "@/components/RechargeForm";
 import ModemDetection from "@/components/modemDetection";
 import TransactionsHistory from "@/components/TransactionsHistory";
@@ -12,7 +12,18 @@ export default function Page() {
     <div className="flex flex-col h-screen bg-gray-50">
       {/* Header */}
       <header className="bg-white h-15 border-b flex items-center px-4 shadow-s ">
-        <h1 className="font-medium text-xl">Flexy Pay</h1>
+        <h1 className="font-medium text-xl flex-1">Flexy Pay</h1>
+        
+          <Tabs defaultValue="home" className="w-[250px] ">
+            <TabsList>
+              <TabsTrigger value="home">Home</TabsTrigger>
+              <TabsTrigger value="offers">Offers</TabsTrigger>
+              <TabsTrigger value="history">History</TabsTrigger>
+            </TabsList>
+            
+          </Tabs>
+        
+        <div className="flex-1"></div>
       </header>
 
       <div className="flex flex-1 gap-4 p-4 overflow-hidden">
