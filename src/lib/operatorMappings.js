0620085a7ee38219b8780d5431a02d36f1bcc8b4
billelK <bilalkhansal@gmail.com>
2025-10-30 06:35:47 +0100

    wip

--- src/lib/operatorMappings.js
+++ src/lib/operatorMappings.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const filePath = path.join(process.cwd(), 'src/operatorPorts.json')
+const filePath = path.join(process.cwd(), 'src/operatorDetectedPorts.json')
 
 export function saveDetectedOperators(operators) {
   try {
