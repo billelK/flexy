1c802c5e6b503e7c2622373699e9f39229c4a8c2
billelK <bilalkhansal@gmail.com>
2025-12-11 17:50:53 +0100

    Remove dist from repository

--- dist/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-console.log("✅ preload.ts is running");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    ping: function () { return Promise.resolve("pong ✅ from preload"); },
-});
